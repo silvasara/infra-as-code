d2bcf0a76bcc0277b1ef2ade154607aaf1849518
Sara Silva <saracsas2@gmail.com>
2020-07-16 16:51:08 -0300

    Fixing interpolation

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
   # region = "us-east-1"
-  region  = "${terraform.workspace == "production" ? "us-east-1" : "us-east-2"}"
+  region  = terraform.workspace == "production" ? "us-east-1" : "us-east-2"
   version = "~> 2.0"
 }
 
